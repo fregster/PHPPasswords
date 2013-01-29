d533ffb33f84782893343418304d1039a836606a
Paul <paul@fryer.org.uk>
2013-01-29 17:40:31 +0000

    Added bcrypt class Minor fixes to get site rendering Update to use setup class Update autoload to use setup class Update to encryption classes to allow for per record salting

--- public_html/index.php
+++ public_html/index.php
@@ -24,5 +24,6 @@
 */
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
+define('SITE_PATH', dirname(__FILE__).'/../');
 define('INC_PATH', dirname(__FILE__).'/..');
 require_once(INC_PATH.'/application/controller.inc');
